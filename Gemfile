7c3181775e3a4c7e3037c889e7807975882b02ee
davidhartman00 <davidhartman00@gmail.com>
2019-05-09 18:28:20 -0700

    added file

--- Gemfile
+++ Gemfile
@@ -42,7 +42,11 @@ gem 'jquery-rails'
 #add bootstrap
 gem 'bootstrap', '~> 4.3.1'
 
+#add twitter
+gem 'twitter'
 
+#add twurl
+gem 'twurl'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "NiMvhoAGUHkuEGIbnNVTKTYHs"
+  config.consumer_secret     = "60sKCvuB5tfRLv1HSqDThamAp8FBVqu8C1Er6CdBtCvGLfpPB9"
+  config.access_token        = "3421039223-nbQ1J74XXKAig85nGWwAqPKciJpNqwKKEU6A1IU"
+  config.access_token_secret = "zd0mvUcneJTc2ojDNFjI5ADrFZhwU8RolhTUwgmFz2iyT"
+end
